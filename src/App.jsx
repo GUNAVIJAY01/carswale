1b2b20bc24fe607ea8c2b031ef4c0c1a29469e38
GUNAVIJAY01 <gunanathan0@gmail.com>
2023-12-30 22:05:59 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Routings } from './Components/Routings/Routing'
+
+function App() {
+  
+
+  return (
+    <>
+      <Routings/>
+         </>
+  )
+}
+
+export default App
